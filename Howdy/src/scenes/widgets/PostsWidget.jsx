dfdd295d9da33094d75aa69a6a66fd1ea18f35c8
Gino Mangalan <ginomangalan@gmail.com>
2024-04-15 17:52:12 -0400

    first commit

---
+++ Howdy/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,84 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../state";
+import PostWidget from "./PostWidget";
+
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+  console.log("-----token ****", token);
+  const getPosts = async () => {
+    console.log("---getPosts--", token);
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+
+  const getUserPosts = async () => {
+    try {
+      console.log("---token--", token);
+      const response = await fetch(
+        `http://localhost:3001/posts/${userId}/posts`,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const data = await response.json();
+      console.log("--getUserPosts data ---", data);
+      dispatch(setPosts({ posts: data }));
+    } catch (err) {
+      console.log("---error in postswidget --", err);
+    }
+  };
+
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  console.log("---posts---", posts);
+  return (
+    <>
+      {posts && Array.isArray(posts) ? (
+        posts?.map(
+          ({
+            _id,
+            userId,
+            firstName,
+            lastName,
+            description,
+            location,
+            picturePath,
+            userPicturePath,
+            likes,
+            comments,
+          }) => (
+            <PostWidget
+              key={_id}
+              postId={_id}
+              postUserId={userId}
+              name={`${firstName} ${lastName}`}
+              description={description}
+              location={location}
+              picturePath={picturePath}
+              userPicturePath={userPicturePath}
+              likes={likes}
+              comments={comments}
+            />
+          )
+        )
+      ) : (
+        <div>No map</div>
+      )}
+    </>
+  );
+};
+
+export default PostsWidget;
